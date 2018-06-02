6f7ad4d32ba75e9ac3bb794659b64ef1efa32c18
Valentine Michael Smith <redditor.vmsmith@gmail.com>
2018-06-02 09:55:29 -0400

    Update code.R

--- code.R
+++ code.R
@@ -1,1 +1,27 @@
+rm(list = ls())
+
+# Best case scenario
+alpha <- 0.50      
+# Worst case scenario
+epsilon <- 0.05  
+
+# Sum of beta, gamma, and delta
+mid_percentages <- 1 - (alpha + epsilon)           
+
+percentages <- seq(0, mid_percentages, .01)
+
+# Create mid-range probabilities  
+# beta
+for(beta in percentages){
+  # gamma
+  for(gamma in percentages){
+    # delta 
+    for(delta in percentages){
+      if(beta + gamma + delta == 0.45){
+        print(paste0(beta, " ", gamma, " ", delta))
+      }
+    }
+  }
+}
+
 
